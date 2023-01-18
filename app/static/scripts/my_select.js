ee4a77135f1c25a6f3f2210e3cbd46589be0ab93
kdevivanco <kdevivanco@gmail.com>
2023-01-17 20:40:08 -0500

    select completamente terminado para el front

--- app/static/scripts/my_select.js
+++ app/static/scripts/my_select.js
@@ -1,23 +1,30 @@
 
-console.log(document.getElementById('my_select'))
+console.log(document.getElementById('my-select'))
 var checkboxes = document.querySelectorAll(".dropdown-content input[type=checkbox]");
+var inputcheckboxes = document.querySelectorAll(".dropdown-content input")
 
 for (var i = 0; i < checkboxes.length; i++) {
-  checkboxes[i].addEventListener("change", checkSelection);
+  console.log(checkboxes.value)
+  inputcheckboxes[i].addEventListener("change", checkSelection);
 }
 
 function checkSelection() {
     var selectedOptions = document.querySelectorAll(".dropdown-content input[type=checkbox]:checked");
     if (selectedOptions.length >= 5) {
-      alert("You can only select a maximum of 5 options");
       var otherCheckboxes = document.querySelectorAll(".dropdown-content input[type=checkbox]:not(:checked)");
       for (var i = 0; i < otherCheckboxes.length; i++) {
         otherCheckboxes[i].disabled = true;
+        this_checkText = document.getElementById("cat-name" + otherCheckboxes[i].value)
+        this_checkText.classList.add('distext')
       }
     } else {
       var allCheckboxes = document.querySelectorAll(".dropdown-content input[type=checkbox]");
       for (var i = 0; i < allCheckboxes.length; i++) {
+        this_checkText = document.getElementById("cat-name" + allCheckboxes[i].value)
+        this_checkText.classList.remove('distext')
+
         allCheckboxes[i].disabled = false;
+
       }
     }
   }
